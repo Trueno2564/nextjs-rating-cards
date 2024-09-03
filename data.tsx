81169c98c8ec64a46e61db37ca5b82b61bc5ec23
Brian Regalado <brianregalado2564@gmail.com>
2024-09-03 14:35:50 -0500

    correcion seccion proyectos

---
+++ data.tsx
@@ -0,0 +1,61 @@
+// import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Rss, Twitch, Youtube, Crop, Pencil, Computer, Book, Rocket, Speech } from "lucide-react";
+// import  Prototipo0 from "./src/assets/images/Prototipo0.png"
+// import imageToAdd from "./../assets/images/logo.png";
+
+export const ProjectsCards = [
+    {
+        id: 1,
+        srcImg: 'https://workana.s3.amazonaws.com/portfolios/cl/596d8f16e8df03bdb30b008995b40b93/Prototipo0.png?response-content-disposition=inline%3Bfilename%3D%22Prototipo0.png%22&response-content-type=image%2Fpng&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA33COQEVTJSIXHA73%2F20240902%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240902T230802Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Signature=d2c803df5e3ce77e126edc71e0d490d7ef5dedd594a463605b19b88addce9756',
+        title: "Prototipo 1",
+        skill1: "PCB Desing",
+        skill2: "PCB Desing",
+        skill3: "PCB Desing",
+        skill4: "PCB Desing",
+        skill5: "PCB Desing",
+        src: "https://www.instagram.com/brian__2564/",
+    },
+    {
+        id: 2,
+        // logo: <Linkedin size={30} strokeWidth={1} />,
+        title: "Prototipo 2",
+        skill1: "PCB Desing",
+        skill2: "PCB Desing",
+        skill3: "PCB Desing",
+        skill4: "PCB Desing",
+        skill5: "PCB Desing",
+        src: "https://www.linkedin.com/in/brianregalado/",
+    },
+    {
+        id: 3,
+        // logo: <Twitter size={30} strokeWidth={1} />,
+        title: "Prototipo 3",
+        skill1: "PCB Desing",
+        skill2: "PCB Desing",
+        skill3: "PCB Desing",
+        skill4: "PCB Desing",
+        skill5: "PCB Desing",
+        src: "https://x.com/BrianRegalado18",
+    },
+    {
+        id: 4,
+        // logo: <Rss size={30} strokeWidth={1} />,
+        title: "Prototipo 4",
+        skill1: "PCB Desing",
+        skill2: "PCB Desing",
+        skill3: "PCB Desing",
+        skill4: "PCB Desing",
+        skill5: "PCB Desing",
+        src: "#!",
+    },
+    {
+        id: 4,
+        // logo: <Twitch size={30} strokeWidth={1} />,
+        title: "Prototipo 5",
+        skill1: "PCB Desing",
+        skill2: "PCB Desing",
+        skill3: "PCB Desing",
+        skill4: "PCB Desing",
+        skill5: "PCB Desing",
+        src: "https://www.twitch.tv/trueno2564",
+    },
+];
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,15 +1,48 @@
 "use client"
 import Modal from '@/components/Modal'
 import React, { Fragment, useState } from 'react'
+import { ProjectsCards } from '../../../data'
+import Link from 'next/link'
 
 function Projects() {
     const [showModal, setShowModal] = useState(false)
     return (
         <Fragment>
-            <div className='p-10 text-center'>
+            <div className='p-10 text-center flex flex-col items-center'>
                 <h1 className='text-3xl mb-5'>Create custom Modal in Next JS with Tailwind CSS</h1>
-                <div className='w-[100px]'>
-                    <img src='https://workana.s3.amazonaws.com/portfolios/cl/596d8f16e8df03bdb30b008995b40b93/Prototipo0.png?response-content-disposition=inline%3Bfilename%3D%22Prototipo0.png%22&response-content-type=image%2Fpng&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA33COQEVTJSIXHA73%2F20240902%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240902T230802Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Signature=d2c803df5e3ce77e126edc71e0d490d7ef5dedd594a463605b19b88addce9756' className='w-fu'/>
+                <div className='flex grid grid-cols-3 justify-items-center gap-10 w-3/4 my-10'>
+                    {/* {[...Array(5)].map(() => {
+                        return (
+                            <button className='text-white bg-neutral-900 hover:bg-neutral-800 shadow-xl transition-shadow shadow-indigo-500/20 focus:outline-none front-medium text-sm rounded-lg mr-5 w-[400px] gap-3 flex flex-col' onClick={() => setShowModal(true)}>
+                            <img src='https://workana.s3.amazonaws.com/portfolios/cl/596d8f16e8df03bdb30b008995b40b93/Prototipo0.png?response-content-disposition=inline%3Bfilename%3D%22Prototipo0.png%22&response-content-type=image%2Fpng&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA33COQEVTJSIXHA73%2F20240902%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240902T230802Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Signature=d2c803df5e3ce77e126edc71e0d490d7ef5dedd594a463605b19b88addce9756' className='w-full rounded-tr-lg rounded-tl-lg'/>
+                            <div className='w-full flex flex-col content-start mx-3'>
+                                <p className='text-2xl font-bold w-auto self-start my-3'>Prototipo</p>
+                                <div className='gap-1 flex flex-wrap mb-3'>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>PCB Desing</div>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>Altium Designer</div>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>Electronica</div>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>3D Model</div>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>Diseno y Fabricacion</div>
+                                </div>
+                            </div>
+                            </button>
+                        )
+                    })} */}
+                    {ProjectsCards.map((item) => (
+                        <button key={item.id} className='text-white bg-neutral-900 hover:bg-neutral-800 shadow-xl transition-shadow shadow-indigo-500/20 focus:outline-none front-medium text-sm rounded-lg mr-5 w-auto gap-3 flex flex-col' onClick={() => setShowModal(true)}>
+                            <img src={item.srcImg} className='w-full rounded-tr-lg rounded-tl-lg'/>
+                            <div className='w-full flex flex-col content-start mx-3'>
+                                <p className='text-2xl font-bold w-auto self-start my-3'>{item.title}</p>
+                                <div className='gap-1 flex flex-wrap mb-3'>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>{item.skill1}</div>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>{item.skill2}</div>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>{item.skill3}</div>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>{item.skill4}</div>
+                                    <div className='text-sm border-solid border-fuchsia-800 border-2 rounded-md grid-flow-dense size-fit px-2'>{item.skill5}</div>
+                                </div>
+                            </div>
+                        </button>
+                    ))}
                 </div>
             </div>
             <Modal isVisible={showModal} onClose={() => setShowModal(false)} className=''>
